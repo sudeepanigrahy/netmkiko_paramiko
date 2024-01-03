726817ea49648547e2886f60a06976b37444ac84
SudeePanigrahy <sudeep.panigrahy10@gmail.com>
2024-01-03 12:33:43 +0530

    Update roughcheckvlan2eos.py

--- splunk_monitoring_project/roughcheckvlan2eos.py
+++ splunk_monitoring_project/roughcheckvlan2eos.py
@@ -23,12 +23,9 @@ def work(connection, switch):
     formatted_time = datetime.fromtimestamp(current_time).strftime("%m-%d-%Y %H:%M:%S") 
 
     """
-    switch = "sg624-xamr0-cpd14-leaf123b"
-    #switch = "sg209-b1mr4d-as1-ah-mn-vpc"
-    #switch = "sg211-frm3-as2-famn301"
-
+    switch = "***********"
     kwargs = {
-        'device_type': 'arista_eos','ip': switch,'username': 'spanigrahy','password': 'Sueme@0125','port':'22'
+        'device_type': 'arista_eos','ip': switch,'username': '***********','password': '***********','port':'22'
         }
     connection = ConnectHandler(**kwargs)
         """
@@ -195,13 +192,13 @@ for i in inputswitchlist:
     print(f"working on {i}...")
     try:
         kwargs = {
-            'device_type': 'arista_eos','ip': i,'username': 'spanigrahy','password': 'Sueme@0125','port':'22'
+            'device_type': 'arista_eos','ip': i,'username': '***********','password': '***********','port':'22'
             }
         connection = ConnectHandler(**kwargs)
     except:
         try:
             kwargs = {
-                'device_type': 'arista_eos_telnet','ip': i,'username': 'spanigrahy','password': 'Sueme@0125','port':'23'
+                'device_type': 'arista_eos_telnet','ip': i,'username': '***********','password': '***********','port':'23'
                 }
             connection = ConnectHandler(**kwargs)
         except:
