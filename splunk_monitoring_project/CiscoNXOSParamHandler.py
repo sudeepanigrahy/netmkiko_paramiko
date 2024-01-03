376426a316aa7cd2c7c2e2d6e638e0460f67d96a
SudeePanigrahy <sudeep.panigrahy10@gmail.com>
2024-01-03 11:54:07 +0530

    Update CiscoNXOSParamHandler.py

--- splunk_monitoring_project/CiscoNXOSParamHandler.py
+++ splunk_monitoring_project/CiscoNXOSParamHandler.py
@@ -3,51 +3,6 @@ import time
 from datetime import datetime
 import pandas as pd
 
-# returns a list of dictionary for each queried device
-def check_crc(i_string, i_hostname):
-    
-    res_dict_list = []
-
-    current_time = time.time()
-    formatted_time = datetime.fromtimestamp(current_time).strftime("%m-%d-%Y %H:%M:%S")    
-    
-    expression_1 = "^.*Ethernet[0-9]+(\/[0-9]+)+.*"
-    expression_2 = "^.*E[0-9]+(\/[0-9]+)+.*"
-    i_string = i_string.replace("\\r\\n", " ")
-    string_split_by_whitespace = i_string.split()
-    
-    cur_index = 0
-    cur_sub_index = 0
-    for entry in string_split_by_whitespace:
-        if(re.search(expression_1, entry) or re.search(expression_2, entry)):
-            input_error = None
-            output_error = None
-            CRC_error = None
-            cur_sub_index = cur_index
-            interface_name = string_split_by_whitespace[cur_index]
-            for sub_entry in string_split_by_whitespace[cur_index:]:
-                if('input' in sub_entry):
-                    input_error = string_split_by_whitespace[cur_sub_index - 1]
-                if('output' in sub_entry):
-                    output_error = string_split_by_whitespace[cur_sub_index - 1]
-                if('CRC' in sub_entry):
-                    CRC_error = string_split_by_whitespace[cur_sub_index - 1]
-                if(input_error != None and output_error != None and CRC_error != None): break
-                cur_sub_index += 1
-            
-            res_dict_list.append({
-                    'timestamp': formatted_time,
-                    'switch name': i_hostname,
-                    'interface name': interface_name,
-                    'CRC error': CRC_error,
-                    'input error': input_error,
-                    'output_error': output_error
-                })
-                
-        cur_index += 1
-
-    return res_dict_list
-
 def check_snmp_config(i_string, i_hostname): 
     #return ['timestamp', 'switch name', 'discrepancy', 'remarks']
     #once this function is called, it'll return the above dict inside a list, to the caller.
@@ -57,28 +12,28 @@ def check_snmp_config(i_string, i_hostname):
     formatted_time = datetime.fromtimestamp(current_time).strftime("%m-%d-%Y %H:%M:%S")        
     res_list = []
     
-    if "sg209" in i_hostname:
-        ip1 = "10.160.20.60"
-        ip2 = "10.160.21.9"
-        if "rc4sing" in i_string and "wc4sing" in i_string and ip1 in i_string and ip2 in i_string:
+    if "xxxxx" in i_hostname:
+        ip1 = "**********"
+        ip2 = "**********"
+        if "**********" in i_string and "**********" in i_string and ip1 in i_string and ip2 in i_string:
             discrepancy = "no"
             remark = ""
         else:
             discrepancy = "yes"
             remark = "SNMP server missing"
-    elif "sg624" in i_hostname:
-        ip1 = "10.193.189.120"
-        ip2 = "10.193.189.26"
-        if "rc4sing" in i_string and "wc4sing" in i_string and ip1 in i_string and ip2 in i_string:
+    elif "xxxxx" in i_hostname:
+        ip1 = "**********"
+        ip2 = "**********"
+        if "**********" in i_string and "**********" in i_string and ip1 in i_string and ip2 in i_string:
             discrepancy = "no"
             remark = ""
         else:
             discrepancy = "yes"
             remark = "SNMP server missing"
-    elif "sg211" in i_hostname:
-        ip1 = "172.25.243.229"
-        ip2 = "172.25.243.230"
-        if "rc4sing" in i_string and "wc4sing" in i_string and ip1 in i_string and ip2 in i_string:
+    elif "xxxxx" in i_hostname:
+        ip1 = "**********"
+        ip2 = "**********"
+        if "**********" in i_string and "**********" in i_string and ip1 in i_string and ip2 in i_string:
             discrepancy = "no"
             remark = ""
         else:
