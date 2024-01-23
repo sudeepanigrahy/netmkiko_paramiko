6c4a7cdd3087f19a1142b1741f5a00eb0bcea23f
SudeePanigrahy <sudeep.panigrahy10@gmail.com>
2024-01-23 17:56:14 +0530

    Update ip_reachability.py

--- acl_checker_generator/ip_reachability.py
+++ acl_checker_generator/ip_reachability.py
@@ -8,14 +8,14 @@ def reachability(device):
     global connection
     try:
         kwargs = {
-            'device_type': 'cisco_ios','ip': device,'username': 'spanigrahy','password': 'Sueme@0128','port':'22'
+            'device_type': 'cisco_ios','ip': device,'username': '*********','password': '*********','port':'22'
             }
         connection = ConnectHandler(**kwargs)
         
     except:
         try:
             kwargs = {
-                'device_type': 'cisco_ios_telnet','ip': device,'username': 'spanigrahy','password': 'Sueme@0128','port':'23'
+                'device_type': 'cisco_ios_telnet','ip': device,'username': '*********','password': '*********','port':'23'
                 }
             connection = ConnectHandler(**kwargs)
         
